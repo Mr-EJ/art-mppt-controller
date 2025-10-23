f7e88a30614b4431b7755d3b7683c368483c4789
Mr-EJ <128086414+Mr-EJ@users.noreply.github.com>
2025-10-22 23:01:40 -0400

    Update services.py

--- components/mppt_controller/services.py
+++ components/mppt_controller/services.py
@@ -1,47 +1,55 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 import esphome.automation as automation
+from esphome.const import CONF_ID
 from . import mppt_ns, MPPTController
 
+# Remove the template parameters from action class definitions
 MPPTResetAction = mppt_ns.class_("MPPTResetAction", automation.Action)
 MPPTSetChargingParamsAction = mppt_ns.class_("MPPTSetChargingParamsAction", automation.Action)
 
-RESET_ENERGY_SCHEMA = cv.Schema({
-    cv.Optional("clear", default=True): cv.boolean,
-    cv.Optional("reboot", default=False): cv.boolean,
-})
-
-SET_CHARGING_SCHEMA = cv.Schema({
-    cv.Required("charge_current"): cv.int_,
-    cv.Required("battery_type"): cv.int_,
-    cv.Required("const_voltage"): cv.int_,
-    cv.Required("load_undervoltage"): cv.int_,
-})
+CONF_CLEAR = 'clear'
+CONF_REBOOT = 'reboot'
+CONF_CHARGE_CURRENT = 'charge_current'
+CONF_BATTERY_TYPE = 'battery_type'
+CONF_CONST_VOLTAGE = 'const_voltage'
+CONF_LOAD_UNDERVOLTAGE = 'load_undervoltage'
 
 @automation.register_action(
     "mppt_controller.reset_energy",
     MPPTResetAction,
-    RESET_ENERGY_SCHEMA
+    cv.Schema({
+        cv.Required(CONF_ID): cv.use_id(MPPTController),
+        cv.Optional(CONF_CLEAR, default=True): cv.boolean,
+        cv.Optional(CONF_REBOOT, default=False): cv.boolean,
+    })
 )
-async def mppt_reset_action(config, action_id, template_arg, args):
-    var = cg.new_Pvariable(action_id)
-    cg.add(var.set_clear(config["clear"]))
-    cg.add(var.set_reboot(config["reboot"]))
+async def mppt_reset_action_to_code(config, action_id, template_arg, args):
+    paren = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, paren)
+    cg.add(var.set_clear(config[CONF_CLEAR]))
+    cg.add(var.set_reboot(config[CONF_REBOOT]))
     return var
 
 @automation.register_action(
     "mppt_controller.set_charging_params",
     MPPTSetChargingParamsAction,
-    SET_CHARGING_SCHEMA
+    cv.Schema({
+        cv.Required(CONF_ID): cv.use_id(MPPTController),
+        cv.Required(CONF_CHARGE_CURRENT): cv.int_range(min=0, max=255),
+        cv.Required(CONF_BATTERY_TYPE): cv.int_range(min=0, max=255),
+        cv.Required(CONF_CONST_VOLTAGE): cv.int_range(min=0, max=65535),
+        cv.Required(CONF_LOAD_UNDERVOLTAGE): cv.int_range(min=0, max=65535),
+    })
 )
-async def mppt_charge_action(config, action_id, template_arg, args):
-    var = cg.new_Pvariable(action_id)
-    cg.add(var.set_charge_current(config["charge_current"]))
-    cg.add(var.set_battery_type(config["battery_type"]))
-    cg.add(var.set_const_voltage(config["const_voltage"]))
-    cg.add(var.set_load_undervoltage(config["load_undervoltage"]))
+async def mppt_set_params_action_to_code(config, action_id, template_arg, args):
+    paren = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, paren)
+    cg.add(var.set_charge_current(config[CONF_CHARGE_CURRENT]))
+    cg.add(var.set_battery_type(config[CONF_BATTERY_TYPE]))
+    cg.add(var.set_const_voltage(config[CONF_CONST_VOLTAGE]))
+    cg.add(var.set_load_undervoltage(config[CONF_LOAD_UNDERVOLTAGE]))
     return var
-    
+
 async def to_code(config):
-    # This function exists only so __init__.py can safely await it
     pass
